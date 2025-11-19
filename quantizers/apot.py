9885250fb1a7e7743fb32dac745cb4cd56c3fd33
Anton Chernov <tony.pitchblack@yandex.ru>
2025-11-18 19:31:59 -0800

    quantizers: add LSQ skeleton and add stubs for PACT, AdaRound, APoT, QIL

--- quantizers/apot.py
+++ quantizers/apot.py
@@ -5,15 +5,15 @@ import torch
 from .base import BaseQuantizer
 
 
-class LSQQuantizer(BaseQuantizer):
+class APoTQuantizer(BaseQuantizer):
     def __init__(self, bit_width: int = 32, per_channel: bool = False):
         super().__init__(bit_width=bit_width, per_channel=per_channel)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError("LSQQuantizer forward not implemented")
+        raise NotImplementedError("APoTQuantizer forward not implemented")
 
     def export_params(self) -> Dict[str, Any]:
-        raise NotImplementedError("LSQQuantizer export_params not implemented")
+        raise NotImplementedError("APoTQuantizer export_params not implemented")
 
 
-# TODO
+
