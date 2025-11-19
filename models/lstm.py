1ac6bdd0f7caa7a748aca32999580d942476f5b9
Anton Chernov <tony.pitchblack@yandex.ru>
2025-11-18 19:31:47 -0800

    models: add ESPCNModel and LSTMModel skeletons

--- models/lstm.py
+++ models/lstm.py
@@ -1,1 +1,11 @@
-# TODO
+import torch
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        raise NotImplementedError
+
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError
+
